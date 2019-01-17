6fd9fe39e6d7e0c190f7a3b9c00d2a169ac4f37a
yonzkon <yonzkon@gmail.com>
2019-01-17 14:20:48 +0800

    network: Use ES6 class

--- packages/@yoda/network/index.js
+++ packages/@yoda/network/index.js
@@ -6,147 +6,148 @@
  */
 
 var EventEmitter = require('events').EventEmitter
-var inherits = require('util').inherits
 var native = require('./network.node')
 
 /**
  * Ping
  */
 
-module.exports.Ping = Ping
+class Ping extends EventEmitter {
+  constructor (address) {
+    super()
 
-function Ping (address) {
-  EventEmitter.call(this)
-  this._intervalFunc = null
-  this._address = 'www.taobao.com'
-  if (address) { this._address = address }
-}
-inherits(Ping, EventEmitter)
+    this._intervalFunc = null
+    this._address = 'www.taobao.com'
+    if (address) { this._address = address }
+  }
 
-Ping.prototype.ping = function (address) {
-  if (!address) { address = this._address }
+  ping (address) {
+    if (!address) { address = this._address }
 
-  if (native.networkState(address) === 0) {
-    return {state: 'CONNECTED'}
-  } else {
-    return {state: 'DISCONNECTED'}
+    if (native.networkState(address) === 0) {
+      return {state: 'CONNECTED'}
+    } else {
+      return {state: 'DISCONNECTED'}
+    }
+  }
+
+  start (interval, address) {
+    if (!address) { address = this._address }
+
+    var fn = function () {
+      if (native.networkState(address) === 0) {
+        this.emit('ping.status', 'ping', {state: 'CONNECTED'})
+      } else {
+        this.emit('ping.status', 'ping', {state: 'DISCONNECTED'})
+      }
+    }.bind(this)
+
+    fn()
+    this._intervalFunc = setInterval(fn, interval)
+  }
+
+  stop () {
+    clearInterval(this._intervalFunc)
   }
 }
 
-Ping.prototype.start = function (interval, address) {
-  if (!address) { address = this._address }
-
-  var fn = function () {
-    if (native.networkState(address) === 0) {
-      this.emit('ping.status', 'ping', {state: 'CONNECTED'})
-    } else {
-      this.emit('ping.status', 'ping', {state: 'DISCONNECTED'})
-    }
-  }.bind(this)
-
-  fn()
-  this._intervalFunc = setInterval(fn, interval)
-}
-
-Ping.prototype.stop = function () {
-  clearInterval(this._intervalFunc)
-}
+module.exports.Ping = Ping
 
 /**
  * Network
  */
 
-module.exports.Network = Network
+class Network extends EventEmitter {
+  constructor (flora) {
+    super()
 
-function Network (flora) {
-  EventEmitter.call(this)
+    this.wifiStatus = {state: 'DISCONNECTED'}
+    this.ethernetStatus = {state: 'DISCONNECTED'}
+    this.modemStatus = {state: 'DISCONNECTED'}
 
-  this.wifiStatus = {state: 'DISCONNECTED'}
-  this.ethernetStatus = {state: 'DISCONNECTED'}
-  this.modemStatus = {state: 'DISCONNECTED'}
+    this._remoteCallTarget = 'net_manager'
+    this._remoteCallCommand = 'network.command'
+    this._remoteCallTimeout = 60 * 1000
 
-  this._remoteCallTarget = 'net_manager'
-  this._remoteCallCommand = 'network.command'
-  this._remoteCallTimeout = 60 * 1000
-
-  this._flora = flora
-  this._initFlora()
-}
-inherits(Network, EventEmitter)
-
-Network.prototype._initFlora = function () {
-  this._flora.subscribe('network.status', (caps, type) => {
-    var msg = JSON.parse(caps[0])
-
-    if (msg.wifi) {
-      this.wifiStatus = msg.wifi
-      this.emit('network.status', 'wifi', msg.wifi)
-    } else if (msg.ethernet) {
-      this.ethernetStatus = msg.ethernet
-      this.emit('network.status', 'ethernet', msg.ethernet)
-    } else if (msg.modem) {
-      this.modemStatus = msg.modem
-      this.emit('network.status', 'modem', msg.modem)
-    }
-  })
-}
-
-Network.prototype._remoteCall = function (device, command, params) {
-  var data = {
-    device: device,
-    command: command
+    this._flora = flora
   }
-  if (params) { data.params = params }
 
-  return this._flora.call(
-    this._remoteCallCommand,
-    [JSON.stringify(data)],
-    this._remoteCallTarget,
-    this._remoteCallTimeout
-  )
+  init () {
+    this._flora.subscribe('network.status', (caps, type) => {
+      var msg = JSON.parse(caps[0])
+
+      if (msg.wifi) {
+        this.wifiStatus = msg.wifi
+        this.emit('network.status', 'wifi', msg.wifi)
+      } else if (msg.ethernet) {
+        this.ethernetStatus = msg.ethernet
+        this.emit('network.status', 'ethernet', msg.ethernet)
+      } else if (msg.modem) {
+        this.modemStatus = msg.modem
+        this.emit('network.status', 'modem', msg.modem)
+      }
+    })
+  }
+
+  _remoteCall (device, command, params) {
+    var data = {
+      device: device,
+      command: command
+    }
+    if (params) { data.params = params }
+
+    return this._flora.call(
+      this._remoteCallCommand,
+      [JSON.stringify(data)],
+      this._remoteCallTarget,
+      this._remoteCallTimeout
+    )
+  }
+
+  capacities () {
+    return this._remoteCall('NETWORK', 'GET_CAPACITY')
+  }
+
+  wifiOpen (ssid, passwd) {
+    return this._remoteCall('WIFI', 'CONNECT', {'SSID': ssid, 'PASSWD': passwd})
+  }
+
+  wifiClose () {
+    return this._remoteCall('WIFI', 'DISCONNECT')
+  }
+
+  wifiStartScan () {
+    return this._remoteCall('WIFI', 'START_SCAN')
+  }
+
+  wifiStopScan () {
+    return this._remoteCall('WIFI', 'STOP_SCAN')
+  }
+
+  wifiScanList () {
+    return this._remoteCall('WIFI', 'GET_WIFILIST')
+  }
+
+  wifiApOpen (ssid, passwd, ip, timeout) {
+    return this._remoteCall('WIFI_AP', 'CONNECT', {
+      SSID: ssid,
+      PASSWD: passwd,
+      IP: ip,
+      TIMEOUT: timeout
+    })
+  }
+
+  wifiApClose () {
+    return this._remoteCall('WIFI_AP', 'DISCONNECT')
+  }
+
+  modemOpen () {
+    return this._remoteCall('MODEM', 'CONNECT')
+  }
+
+  modemClose () {
+    return this._remoteCall('MODEM', 'DISCONNECT')
+  }
 }
 
-Network.prototype.capacities = function () {
-  return this._remoteCall('NETWORK', 'GET_CAPACITY')
-}
-
-Network.prototype.wifiOpen = function (ssid, passwd) {
-  return this._remoteCall('WIFI', 'CONNECT', {'SSID': ssid, 'PASSWD': passwd})
-}
-
-Network.prototype.wifiClose = function () {
-  return this._remoteCall('WIFI', 'DISCONNECT')
-}
-
-Network.prototype.wifiStartScan = function () {
-  return this._remoteCall('WIFI', 'START_SCAN')
-}
-
-Network.prototype.wifiStopScan = function () {
-  return this._remoteCall('WIFI', 'STOP_SCAN')
-}
-
-Network.prototype.wifiScanList = function () {
-  return this._remoteCall('WIFI', 'GET_WIFILIST')
-}
-
-Network.prototype.wifiApOpen = function (ssid, passwd, ip, timeout) {
-  return this._remoteCall('WIFI_AP', 'CONNECT', {
-    SSID: ssid,
-    PASSWD: passwd,
-    IP: ip,
-    TIMEOUT: timeout
-  })
-}
-
-Network.prototype.wifiApClose = function () {
-  return this._remoteCall('WIFI_AP', 'DISCONNECT')
-}
-
-Network.prototype.modemOpen = function () {
-  return this._remoteCall('MODEM', 'CONNECT')
-}
-
-Network.prototype.modemClose = function () {
-  return this._remoteCall('MODEM', 'DISCONNECT')
-}
+module.exports.Network = Network
